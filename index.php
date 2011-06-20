6bde92e958adb4331bdabc8aaff936febca1a61d
ms <spam@example.com>
2011-06-20 23:54:04 +0200

    Tabellenspalten alternieren

--- index.php
+++ index.php
@@ -37,41 +37,45 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);		//@todo das steht 
 	</div>
 	<div>
 		<!-- aus jeder kategorie 7-8 ausgeben. div drum, werte in table -->
-		<?php 
-		$c=new pCategory();
-		$c->dbh($dbh);
-		$categories=$c->mysqlSelect();
-		
+	<?php
+	$c=new pCategory();
+	$c->dbh($dbh);
+	$categories=$c->mysqlSelect();
 
-		foreach ($categories as $c){
+
+	foreach ($categories as $c){
 
 
 		//-----lsExploits--------------------------------------------------------------------------------------------------------------------------------------------------------------
 		$e= new pExploit();
 		$e->dbh($dbh);
-		$exploits=$e->mySqlSelectByCategory($c->id(),0, 8);		
+		$exploits=$e->mySqlSelectByCategory($c->id(),0, 8);
 		echo "<div class=\"exploit-category\">\n";
 		echo "<h4 class=\"category-title\">{$c->name()}</h4><table>\n";
-		foreach ($exploits as $e){
+		$ctr=0;
+		foreach ($exploits as $e){			
+			$ctr%2==0 ? $modulo="table-gerade" : $modulo="table-ungerade";
 			$f=new Formgen();
-	
+
 			$viewExploit=$f->getLink($e->title(), "ShowExploit.php", array("view"=> $e->id()));
-			$viewByAuthor=$f->getLink($e->autor(), "index.php", array("id"=>-1));	
+			$viewByAuthor=$f->getLink($e->autor(), "index.php", array("id"=>-1));
 			$viewByPlatform=$f->getLink($e->loadPlatform(), "index.php", array("id"=>-1));
-			
+
 			$download="";
-				if ($e->file()!='')
-					$download=$f->getLink('&#9112;', $e->file());
+			if ($e->file()!='')
+			$download=$f->getLink('&#9112;', $e->file());
 			$verified="&#10003;";
-				if ($e->verified())		
-					$verified="&#10006;";		
-				
-			echo "<tr><td>{$e->date()}</td><td>$download</td><td>$verified</td><td>$viewExploit</a></td><td>{$e->hits()}</td><td>$viewByPlatform</td><td>$viewByAuthor</td></tr>";
+			if ($e->verified())
+			$verified="&#10006;";
+
+			echo "<tr class=\"$modulo\"><td>{$e->date()}</td><td>$download</td><td>$verified</td><td>$viewExploit</a></td><td>{$e->hits()}</td><td>$viewByPlatform</td><td>$viewByAuthor</td></tr>\n";
+			$ctr++;
 		}//each
-		echo "</table></div>";
-		//-----lsExploits--------------------------------------------------------------------------------------------------------------------------------------------------------------		
-		}
-		?>
+		echo "</table></div>\n";
+		
+		//-----lsExploits--------------------------------------------------------------------------------------------------------------------------------------------------------------
+	}
+	?>
 	</div>
 
 </body>
