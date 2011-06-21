4f221ca44bf0f483beeadc9a4a9bc890db9b7a67
ms <spam@example.com>
2011-06-21 19:03:04 +0200

    ToDo Liste angepasst

--- TODO.php
+++ TODO.php
@@ -3,12 +3,16 @@
  *Programmieren
  *----
  *@todo: Admin Panel -- Layout, dann die einzelnen Module und ferner evtl. hinzufügen/löschen von Modulen (PHP (Dateien, Klassen), MySQL etc.
- *
- *@todo: Pastebin (open Source) auseinander nehmen und so gestalten, dass Random-File Uploads funktionieren. Sowie den Rest für das Modul.
+
  * 
  *@todo: Module + Templates
  * 
  *Müssen andere später machen
+ *
+ *
+ *@todo suche implementieren
+ *@todo gedanken zur userverwaltung - vor allem wg. anonymen hochladen
+ *@todo fakesichererer hitcounter
  *---------------------------
  *@todo: mod-rewrite
 */
